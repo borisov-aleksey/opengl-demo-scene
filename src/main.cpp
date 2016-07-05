becf2a6a477f9ee596f125c0fa1e9b4e067e30ce
Nikita Rodionov <NekoNekt@gmail.com>
2016-07-05 22:04:10 +0300

    Add ifdef for OS X opengl includes.

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include <SDL.h>
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif // __APPLE__
 
 const int width = 640;
 const int height = 480;
